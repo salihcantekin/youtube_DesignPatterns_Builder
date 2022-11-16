d0c2fd81babc5650fc0b323aa2a9e4651714c3b5
Salih Cantekin <salihcantekin@gmail.com>
2022-11-16 22:34:13 +0000

    SingletonPatterrn

---
+++ DesignPatterns.SingletonPattern/CountryProvider.cs
@@ -0,0 +1,31 @@
+﻿using DesignPatterns.SingletonPattern.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DesignPatterns.SingletonPattern;
+public class CountryProvider
+{
+    private static CountryProvider instance = null;
+    public static CountryProvider Instance => instance ?? (instance = new CountryProvider());
+
+    private new List<Country> Countries { get; set; }
+
+    private CountryProvider()
+    {
+        // Retrieving data from db
+        Task.Delay(2000).GetAwaiter().GetResult();
+
+        Countries = new List<Country>()
+        {
+            new Country(){ Name = "Türkiye" },
+            new Country(){ Name = "Birleşik Krallık" }
+        };
+    }
+
+    public int CountryCount => Countries.Count;
+
+    public async Task<List<Country>> GetCountries() => Countries;
+}
---
+++ DesignPatterns.SingletonPattern/Program.cs
@@ -0,0 +1,36 @@
+﻿// Singleton Design Pattern
+using System;
+using DesignPatterns.SingletonPattern;
+
+#region Explanation
+
+/* *** Pros ***
+    - Ensure single instance (can be use for memory caching)
+    - Globally Access
+    - Created only when requested
+*/
+
+/* *** Cons ***
+    - Difficult to UnitTest (mocking)
+    - In multithread word, threads cannot create its own instance
+*/
+
+#endregion
+
+
+Console.WriteLine(DateTime.Now.ToLongTimeString());
+var countries = await CountryProvider.Instance.GetCountries();
+
+
+//foreach (var country in countries)
+//{
+//    Console.WriteLine(country.Name);
+//}
+
+// Another service
+
+var countries1 = await CountryProvider.Instance.GetCountries();
+
+Console.WriteLine(CountryProvider.Instance.CountryCount);
+
+Console.WriteLine(DateTime.Now.ToLongTimeString());
